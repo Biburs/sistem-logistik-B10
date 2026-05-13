b4e756f5efd5c10697e27e21f0a36f7579639847
Biburs <ihsanhabiburrohim123@gmail.com>
2026-05-13 21:02:37 +0700

    Ihsan Auliya Habiburrohim : Menambahkan fitur authentication dan login

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,10 @@
+const authMiddleware = (req, res, next) => {
+
+    if (!req.session.user) {
+        return res.redirect('/login');
+    }
+
+    next();
+};
+
+module.exports = authMiddleware;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Server berjalan');
-});
+const authRoutes = require("./authRoutes");
+
+router.use(authRoutes);
 
 module.exports = router;
